4776283bba3c0d076fb041eedae23da6d83204e3
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-10-07 22:31:31 -0300

    Added more test cases

--- BitonicSequenceTest.hs
+++ BitonicSequenceTest.hs
@@ -10,11 +10,29 @@ testBasicCase = TestCase $ do
     let expected = [9, 10, 9, 8, 7]
     assertEqual "bitonicArray 5 3 10 should return [9, 10, 9, 8, 7]" expected result
 
+testOtherBasicCase :: Test
+testOtherBasicCase = TestCase $ do
+    let result = bitonicArray 5 3 10
+    let expected = [9, 10, 9, 8, 7]
+    assertEqual "bitonicArray 5 3 10 should return [9, 10, 9, 8, 7]" expected result
+
+testImpossibleCase :: Test
+testImpossibleCase = TestCase $ do
+    let result = bitonicArray 7 2 5
+    assertEqual "bitonicArray 7 2 5 should return [2, 3, 4, 5, 4, 3, 2] (impossible)" [2, 3, 4, 5, 4, 3, 2] result
+
+tests :: Test
+tests = TestList
+    [ TestLabel "Basic case" testBasicCase
+    , TestLabel "Impossible case" testImpossibleCase
+    , TestLabel "Other basic case" testOtherBasicCase
+    ]
+
 -- Main function to run the test
 main :: IO ()
 main = do
-    putStrLn "Running Bitonic Sequence Generator Test"
-    counts <- runTestTT testBasicCase
+    putStrLn "Running Bitonic Sequence Generator Tests"
+    counts <- runTestTT tests
     putStrLn ""
     if errors counts + failures counts == 0
         then do
